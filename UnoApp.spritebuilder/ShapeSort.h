8d2eb5f5d6755431767c7093ef4021da61ba1cb6
yxtonic <yxtonic@gmail.com>
2014-10-21 18:42:54 -0700

    Added shape sort game mode

---
+++ UnoApp.spritebuilder/ShapeSort.h
@@ -0,0 +1,13 @@
+//
+//  ShapeSort.h
+//  UnoApp
+//
+//  Created by Toni Chen on 10/21/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface ShapeSort : CCNode
+
+@end
